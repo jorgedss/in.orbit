f6b8d038cacfaf37d646dfb0195197f40ab92f37
jorgedss <jorgesouza621@gmail.com>
2024-09-10 21:45:40 -0300

    configurações do banco de dados

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log('gjge dvgj')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
